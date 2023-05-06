5dc3b4f022ef652c3d023f7b1afe87fc9e7d4f2c
Youssef_Saad <6270@holbertonstudents.com>
2023-05-06 20:47:24 +0100

    simple shell project files

---
+++ execute.c
@@ -0,0 +1,28 @@
+#include"myshell.h"
+
+/**
+ * exec_cmd - function execute the commande
+ * @cmd: the commande to execute
+ */
+
+void exec_cmd(char **cmd)
+{
+	pid_t	pid = 0;
+	int		status = 0;
+
+	pid = fork();
+	if (pid == -1)
+		perror("fork");
+	
+	else if (pid > 0)
+	{
+		waitpid(pid, &status, 0);
+		kill(pid, SIGTERM);
+	}
+	else
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+			perror("shell");
+		exit(EXIT_FAILURE);
+	}
+}
